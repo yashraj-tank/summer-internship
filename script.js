de66ac13de6686af587d337823a6e7f30be428e8
yashrajtank <yashrajtank@gmai.com>
2026-07-03 19:13:23 +0530

    day 1 introduction

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+
+    document.getElementById("demo").innerHTML =
+    "JavaScript successfully handled the button click and updated the webpage using DOM Manipulation.";
+
+}
