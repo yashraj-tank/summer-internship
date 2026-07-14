f50f9994aed033d51e7008d528bcb53b7547728a
yashrajtank <yashrajtank@gmai.com>
2026-07-14 20:00:11 +0530

    javascript functions

--- script.js
+++ script.js
@@ -50,3 +50,46 @@ function printNumbers(){
     document.getElementById("output").innerHTML=text;
 
 }
+// Function without Parameters
+function greetUser(){
+
+    document.getElementById("output").innerHTML =
+    "Hello! Welcome to JavaScript Functions.";
+
+}
+
+// Function with Parameters
+function addNumbers(num1,num2){
+
+    let sum=num1+num2;
+
+    document.getElementById("output").innerHTML =
+    "Sum = " + sum;
+
+}
+
+// Function using Object
+function showStudent(){
+
+    let student={
+        name:"Yashraj",
+        course:"Web Development",
+        city:"Ahmedabad"
+    };
+
+    document.getElementById("output").innerHTML =
+    "Name: " + student.name +
+    "<br>Course: " + student.course +
+    "<br>City: " + student.city;
+
+}
+
+// Function using Built-in Date Object
+function showDate(){
+
+    let today=new Date();
+
+    document.getElementById("output").innerHTML =
+    today;
+
+}
